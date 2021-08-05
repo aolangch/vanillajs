1ff4e3ef7ab9d8b58d75e408201f53d49f5c7be3
Lang <49438860+aolangch@users.noreply.github.com>
2021-08-05 16:41:55 +0700

    Add files via upload

---
+++ datatypes/datatypes.js
@@ -0,0 +1,11 @@
+IceCream0 = "Hello";
+console.log(typeof IceCream0);
+
+const IceCream1 = 5;
+console.log(typeof IceCream1);
+
+var IceCream2 = true;
+console.log(typeof IceCream2);
+ 
+let x = 5 + "IceCream";
+console.log(x);
